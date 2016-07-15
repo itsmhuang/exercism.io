d7a7bd932eded9a3344ce6d41b8f380780b1beeb
Michelle Huang <mhuangsmailbox@gmail.com>
2016-07-15 10:20:42 -0700

    use slice and trim

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,20 +1,19 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-	//remove spaces from input
-	var noSpaces = input.replace(/\s/g, "");
-	//then remove numbers and comma
-	var letters = noSpaces.replace(/[0-9,]/g, "");
+	//remove spaces, numbers and comma
+	var letters = input.replace(/\d|,|\s/g, "");
+	
 	//no characters, nothing or just spaces
-	if (!input.replace(/\s/g, "").length) {
-		return "Fine. Be that way!"
+	if(!input.trim()) {
+		return "Fine. Be that way!";
 	}
  	//all letters are uppercase
 	else if(letters.length > 1 && letters === letters.toUpperCase()) {
-		return "Whoa, chill out!"
+		return "Whoa, chill out!";
 	}	
 	//ask question - ends with ?
-	else if(input.charAt(input.length-1) === "?") {
+	else if(input.slice(-1) === "?") {
 		return "Sure.";
 	}
 	//everything else, return whatever
